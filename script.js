a513662728a4798b333359379a92a9e42d94f700
Elizeu Junior <131943477+elizeujrdev@users.noreply.github.com>
2023-04-30 10:10:25 -0300

    imagem no mobile concertada

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const menu = document.querySelector('.menu');
 const NavMenu = document.querySelector('.nav-menu');
+const img = document.getElementById('img')
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
+
+    if (menu.classList.contains('ativo')) {
+
+        img.style.display = 'none'
+
+    } else {
+
+        img.style.display = 'block'
+
+    }
 })
